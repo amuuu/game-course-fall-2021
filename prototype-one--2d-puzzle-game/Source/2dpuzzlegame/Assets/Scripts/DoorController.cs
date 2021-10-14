5b6c27d44faf26fd6156f8c688a6abd2d0286bc3
AmiraliPak <a4amirali@hotmail.com>
2021-10-14 19:34:31 +0330

    feat(levelObjective): add door and opening logic

---
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/DoorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public int NumberOfKeysNeeded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool OpenDoor(int keysCount){
+        Debug.Log(string.Format("need {0} keys to open the door",NumberOfKeysNeeded));
+
+        if (keysCount >= NumberOfKeysNeeded){
+            Debug.Log("door opened");
+            //invoke event to show win text
+            return true;
+        }
+        Debug.Log("keys not enough!");
+        return false;
+    }
+
+    //method to show needed keys count on top of door
+}
