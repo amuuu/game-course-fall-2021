cdc80bd3c9e8e0ac99bd183ec96eab5c418ef445
mohammad javad taheri <tmoj.1428@gmail.com>
2021-10-15 13:17:19 +0330

    Add key number text in UI

--- prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/UiManager.cs
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/UiManager.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class UiManager : MonoBehaviour
 {
     public Text counterText;
+    public Text keyNumberText;          // Reference to key number text in UI
     public EventSystemCustom eventSystem;
 
     void Start()
     {
         eventSystem.OnCloneStickyPlatformEnter.AddListener(UpdateScoreText);
+        eventSystem.onGetKey.AddListener(UpdateKeyNumberText);
     }
 
     public void UpdateScoreText()
@@ -18,5 +20,12 @@ public class UiManager : MonoBehaviour
         Debug.Log("UPDATE SCORE");
         int newTextValue = int.Parse(counterText.text) + 1;
             counterText.text = newTextValue.ToString();
+    }
+
+    // Update key number text in UI
+    public void UpdateKeyNumberText()
+    {
+        int newKeyValue = int.Parse(keyNumberText.text) + 1;
+        keyNumberText.text = newKeyValue.ToString();
     }
 }
