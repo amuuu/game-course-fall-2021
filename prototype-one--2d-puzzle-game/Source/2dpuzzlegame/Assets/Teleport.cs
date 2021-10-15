e08336a46b8d4314fe305954216e6b123a7a026f
mohammad javad taheri <tmoj.1428@gmail.com>
2021-10-15 18:54:42 +0330

    Add teleport functionality

--- prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Teleport.cs
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Teleport.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Teleport : MonoBehaviour
 {
     public float teleportDoorDist;
 
     public GameObject Player;
+    public GameObject appearPoint;
     public GameObject destinationDoor;
 
     void Start()
@@ -17,11 +17,18 @@ public class Teleport : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            float distFromTeleDoor = Vector3.Distance(Player.transform.position, transform.position);
-            if (distFromTeleDoor < teleportDoorDist)
+            if (Player.GetComponent<CollectItem>().teleportKeyNumber > 0)
             {
-
-                Debug.Log("Here near the source teleport door");
+                float distFromTeleDoor = Vector3.Distance(Player.transform.position, transform.position);
+                if (distFromTeleDoor < teleportDoorDist)
+                {
+                    Player.SetActive(false);
+                    Player.transform.position = appearPoint.transform.position;
+                    Player.SetActive(true);
+                }
+            }else
+            {
+                Debug.Log("You don't have the key");
             }
         }
     }
