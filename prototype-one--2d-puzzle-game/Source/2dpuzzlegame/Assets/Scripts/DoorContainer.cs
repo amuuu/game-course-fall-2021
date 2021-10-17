e2a1e1dfa8d76ddd8c7f9187407edf77abbb7b5a
Sepehr Babapour <Babapour.Sepehr@gmail.com>
2021-10-17 22:55:07 +0330

    Add attributes to door container

---
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/DoorContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorContainer : MonoBehaviour
+{
+    public GameObject otherDoor;
+    public bool isSource;
+}
