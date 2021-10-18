8a19ec3eddff71a1d7d6f00413a745d5860eff20
Mohammad mahdi Soori <mohammadmahdisoori@gmail.com>
2021-10-18 14:06:16 +0330

    mohammad mahdi soori - complete task of 1 to 4 in HW 1

---
+++ prototype-one--2d-puzzle-game/Source/New Unity Project/Assets/Scripts/EventSystemCustom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventSystemCustom : MonoBehaviour
+{
+    public UnityEvent OnCloneStickyPlatformEnter;
+    public UnityEvent OnKeyCollected;
+    public UnityEvent OnPlayerWin;
+    public UnityEvent OnPlayerLoose;
+
+    void Awake()
+    {
+        OnCloneStickyPlatformEnter = new UnityEvent();
+        OnKeyCollected = new UnityEvent();
+        OnPlayerWin = new UnityEvent();
+        OnPlayerLoose = new UnityEvent();
+    }
+}
---
+++ prototype-one--2d-puzzle-game/Source/New Unity Project/Assets/Scripts/UiManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public Text counterText;
+    public Text keyCollectedText;
+	public Text winOrLooseText;
+
+    public EventSystemCustom eventSystem;
+
+    void Start()
+    {
+        eventSystem.OnCloneStickyPlatformEnter.AddListener(UpdateScoreText);
+		eventSystem.OnKeyCollected.AddListener(UpdateKeyCollectedText);
+		eventSystem.OnPlayerWin.AddListener(UpdateWinText);
+		eventSystem.OnPlayerLoose.AddListener(UpdateLooseText);
+    }
+
+    public void UpdateScoreText()
+    {
+        Debug.Log("UPDATE SCORE");
+        int newTextValue = int.Parse(counterText.text) + 1;
+            counterText.text = newTextValue.ToString();
+    }
+
+	public void UpdateKeyCollectedText()
+    {
+        Debug.Log("UPDATE Key");
+        int newTextValue = int.Parse(keyCollectedText.text) + 1;
+            keyCollectedText.text = newTextValue.ToString();
+    }
+
+	public void UpdateWinText()
+    {
+        Debug.Log("UPDATE Win");
+		winOrLooseText.text = "You Won!";
+    }
+	public void UpdateLooseText()
+    {
+        Debug.Log("UPDATE Loose");
+		winOrLooseText.text = "You Lost!";
+    }
+}
