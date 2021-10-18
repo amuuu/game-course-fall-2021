78c4dbe28cdc328c917b08e82d23d93f8586aba2
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-10-18 16:08:44 +0330

    Implement teleport mechanic for player

---
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/PortalSource.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalSource : MonoBehaviour
+{
+    public GameObject destination;
+    GameObject player;
+    bool playerIsNearby;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerIsNearby = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsNearby)
+        {
+            player.transform.position = destination.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(TagNames.Player.ToString()))
+        {
+            player = collision.gameObject;
+            playerIsNearby = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(TagNames.Player.ToString()))
+        {
+            player = null;
+            playerIsNearby = false;
+        }
+    }
+}
