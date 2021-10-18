dbc0064c121f898edc8b94076df99f43ed6b5b91
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-10-18 21:40:44 +0330

    Add script to store data in level

---
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public EventSystemCustom eventSystem;
+    public int portalKeyCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        portalKeyCount = 0;
+    }
+}
