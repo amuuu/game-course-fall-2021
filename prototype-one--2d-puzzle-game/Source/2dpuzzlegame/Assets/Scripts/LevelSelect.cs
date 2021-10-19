9e1ecff70c353c2120909c4dbaec307c2a17211b
AmiraliPak <a4amirali@hotmail.com>
2021-10-19 22:32:15 +0330

    feat(levelSelection): add level selection menu

---
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void LoadLevel(int levelNumber)
+    {
+        SceneManager.LoadScene("Level " + levelNumber);
+    }
+}
