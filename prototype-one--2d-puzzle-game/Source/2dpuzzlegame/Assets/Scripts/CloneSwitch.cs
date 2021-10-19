d4aba73184b114add2d69ace518fba59b7e29982
Diyar Hamedi <diyarhamedi81@gmail.com>
2021-10-19 14:43:35 +0330

    Implement switch pickup

---
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/CloneSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneSwitch : MonoBehaviour
+{
+    bool playerIsNearby;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerIsNearby = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsNearby)
+        {
+            Debug.Log("SWITCH");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(TagNames.Player.ToString()))
+        {
+            playerIsNearby = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(TagNames.Player.ToString()))
+        {
+            playerIsNearby = false;
+        }
+    }
+}
