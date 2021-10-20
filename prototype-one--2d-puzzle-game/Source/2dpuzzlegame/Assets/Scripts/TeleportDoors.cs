3d4e6ee55b2d02877a85acae4471076c13379a9c
Soheil Hoseini <soheil.hosseini78@gmail.com>
2021-10-21 00:55:49 +0330

    Part 5: Boolean and reference to destination door added.

---
+++ prototype-one--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/TeleportDoors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportDoors : MonoBehaviour
+{
+    [SerializeField] bool isSourceDoor;
+    [SerializeField] GameObject destinationDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
