b38e76272708d55727fcd56fe5afe80cf6e96133
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-10-24 01:19:40 +0330

    Add Life giving wine XD

---
+++ prototype-two--food-game/Source/foodgameprj/Assets/Scripts/LifeGivingWineController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeGivingWineController : ComboInstanceController
+{
+    public override void OnConsume(PlayerController player)
+    {
+        Debug.Log("You ate life giving wine!");
+        player.playerHealth++;
+    }
+}
