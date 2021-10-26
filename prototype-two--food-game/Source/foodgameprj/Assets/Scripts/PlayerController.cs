f0dd31b7546af47bae9983cf98cbb0076c6b5149
AmiraliPak <a4amirali@hotmail.com>
2021-10-27 00:05:31 +0330

    fix: fix movement and combo kill

--- prototype-two--food-game/Source/foodgameprj/Assets/Scripts/PlayerController.cs
+++ prototype-two--food-game/Source/foodgameprj/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Range(0f, 1f)] public float moveAmount;
+    [Range(0f, 10f)] public float moveAmount;
 
     public int playerScore;
     public int playerHeartsCount;
@@ -16,14 +16,13 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(moveAmount, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(-moveAmount, 0, 0);
-        }
+        MovementHandler();
+    }
+
+    private void MovementHandler()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        transform.Translate(movementX * moveAmount * Time.deltaTime, 0, 0, Space.Self);
     }
 
     private void OnCollisionEnter(Collision collision)
