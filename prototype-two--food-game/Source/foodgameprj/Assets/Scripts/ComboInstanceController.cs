fa7b1d68056cd3be9bf4781ac7345ca122a82627
sam astaraki <astarakeesam@gmail.com>
2021-10-27 15:15:23 +0330

    Hello

---
+++ prototype-two--food-game/Source/foodgameprj/Assets/Scripts/ComboInstanceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboInstanceController : MonoBehaviour
+{
+    public ComboItemConfig config;
+
+
+    // when player eats the combo item
+    public virtual void OnConsume()
+    {
+        
+        Debug.Log("PARENT CLASS ON CONSUME");
+    }
+}
---
+++ prototype-two--food-game/Source/foodgameprj/Assets/Scripts/ComboItemConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ComboItemConfig", menuName = "FoodGameConfigs/ComboItemConfig")]
+public class ComboItemConfig : ScriptableObject
+{
+    public string comboName;
+ 
+}
