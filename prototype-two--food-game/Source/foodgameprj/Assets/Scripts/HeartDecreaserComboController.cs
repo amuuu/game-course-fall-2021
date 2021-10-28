ad1531fd2646fdd69bb5aacdd5dd7af848fe26ee
Yasmin <58246152+yasmin-mdn@users.noreply.github.com>
2021-10-28 18:12:57 +0330

    ADD 2type combo

---
+++ prototype-two--food-game/Source/foodgameprj/Assets/Scripts/HeartDecreaserComboController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartDecreaserComboController : ComboInstanceController
+{
+    public override void OnConsume()
+    {
+        Debug.Log("Heart Decrease ON CONSUME");
+
+        // you should fill this method!
+    }
+}
---
+++ prototype-two--food-game/Source/foodgameprj/Assets/Scripts/HeartIncreaserComboController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartIncreaserComboController : ComboInstanceController
+{
+    public override void OnConsume()
+    {
+        Debug.Log("Heart Increase ON CONSUME");
+
+        // you should fill this method!
+    }
+
+}
