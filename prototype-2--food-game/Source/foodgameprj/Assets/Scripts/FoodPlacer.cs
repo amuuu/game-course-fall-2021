36bd6ab2423930b22152d9d138798e0116b26ee6
mahdasjs <45814320+mahdasjs@users.noreply.github.com>
2021-10-29 22:32:02 +0330

    by increasing score plate moves faster

--- prototype-2--food-game/Source/foodgameprj/Assets/Scripts/FoodPlacer.cs
+++ prototype-2--food-game/Source/foodgameprj/Assets/Scripts/FoodPlacer.cs
@@ -52,7 +52,7 @@ public class FoodPlacer : MonoBehaviour
 
     private void UpdateTimerValueBasedOnScore()
     {
-        if ( playerController.playerScore%400 <200 && playerController.playerScore % 400 >=0)
+        if ( playerController.playerScore%400 <200 && playerController.playerScore % 400 >=0 && 400<playerController.playerScore)
         {
             timerMaxTime -= 0.05f;
             playerController.moveAmount += 0.01f;
