b1e0777ec321bd7de64c4af99c9457a8d858c86e
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2021-10-29 15:53:29 +0330

    restructured folders and added new combo contoller

---
+++ prototype-2--food-game/Source/foodgameprj/Assets/Scripts/ComboController/HealthComboController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComboController : ComboInstanceController
+{
+    // when player eats the combo item
+    public override void OnConsume()
+    {
+        Debug.Log("Health combo ON CONSUME");
+
+        // you should fill this method!
+    }
+}
