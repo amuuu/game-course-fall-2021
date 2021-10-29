07c5a83a40c5b438bdf5ec462b00dad20e70d3fc
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2021-10-29 15:48:27 +0330

    restructured folders and added new configs for different combos

---
+++ prototype-2--food-game/Source/foodgameprj/Assets/Scripts/HealthComboItemConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HealthComboItemConfig", menuName = "FoodGameConfigs/ComboItemConfig/HealthComboItemConfig")]
+public class HealthComboItemConfig : ComboItemConfig
+{
+    public int healthChange;
+}
