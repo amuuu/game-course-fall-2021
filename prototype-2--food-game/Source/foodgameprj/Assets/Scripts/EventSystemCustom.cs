f461a6305a836b5182b1b3002128551fb4a84763
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2021-10-29 18:46:25 +0330

    added canvas and some ui text

---
+++ prototype-2--food-game/Source/foodgameprj/Assets/Scripts/EventSystemCustom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class MyIntEvent : UnityEvent<int> {}
+[System.Serializable]
+public class MyTextEvent : UnityEvent<string> {}
+
+
+public class EventSystemCustom : MonoBehaviour
+{
+    public static EventSystemCustom current;
+    public UnityEvent OnCloneStickyPlatformEnter;
+    public MyIntEvent onKeyCollect;
+    public MyTextEvent onHintChange;
+    public MyTextEvent onEndGame;
+
+    void Awake()
+    {
+        current = this;
+        OnCloneStickyPlatformEnter = new UnityEvent();
+        onKeyCollect = new MyIntEvent();
+        onHintChange = new MyTextEvent();
+        onEndGame = new MyTextEvent();
+    }
+}
