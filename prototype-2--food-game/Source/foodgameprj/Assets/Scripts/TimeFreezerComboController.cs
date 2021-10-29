fb8134dd193ce996fa8c7e7be6ae77356f9f7a05
AmiraliPak <a4amirali@hotmail.com>
2021-10-29 18:25:52 +0330

    feat(timeFreezer): add timeFreezer logic and prefab mesh

--- prototype-2--food-game/Source/foodgameprj/Assets/Scripts/TimeFreezerComboController.cs
+++ prototype-2--food-game/Source/foodgameprj/Assets/Scripts/TimeFreezerComboController.cs
@@ -1,14 +1,50 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeFreezerComboController : ComboInstanceController
 {
+    [Range(0.0f, 1.0f)] public float changeRate;
+    bool actionDone = false;
+    void Start()
+    {
+        AutoDestroyOnConsume = false;
+    }
+    void Update()
+    {
+        if (actionDone) DestroyCombo();
+    }
+
     // when player eats the combo item
-    public override void OnConsume(PlayerController playerController)
+    protected override void OnConsumeAction(PlayerController playerController)
     {
         Debug.Log("TIME FREEZER ON CONSUME");
+        
+        GetComponent<Collider>().enabled = false;
+        GetComponentInChildren<MeshRenderer>().enabled = false;
 
-        // you should fill this method!
+        StartCoroutine(ChangeTimeScaleAndDestroy());
+    }
+
+    IEnumerator ChangeTimeScaleAndDestroy()
+    {
+        float newTimeScale = Time.timeScale;
+
+        while(newTimeScale > 0f)
+        {
+            newTimeScale =  Time.timeScale - changeRate*Time.unscaledDeltaTime;
+            if (newTimeScale < 0f) newTimeScale = 0f;
+            Time.timeScale = newTimeScale;
+            yield return null;
+        }
+
+        while(newTimeScale < 1f)
+        {
+            newTimeScale = Time.timeScale + changeRate*Time.unscaledDeltaTime;
+            if (newTimeScale > 1f) newTimeScale = 1f;
+            Time.timeScale = newTimeScale;
+            yield return null;
+        }
+    }
     }
 }
