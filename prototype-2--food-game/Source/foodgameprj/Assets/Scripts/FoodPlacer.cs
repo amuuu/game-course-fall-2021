78f00beb61a9d2441bcab61f1c1edf2a996d4199
Melika Ahmadi Ranjbar <melika.ahmadi2000@yahoo.com>
2021-10-29 13:16:31 +0330

    Melika Ahmadi Ranjbar 4 Done Freezer Left

--- prototype-2--food-game/Source/foodgameprj/Assets/Scripts/FoodPlacer.cs
+++ prototype-2--food-game/Source/foodgameprj/Assets/Scripts/FoodPlacer.cs
@@ -34,7 +34,7 @@ public class FoodPlacer : MonoBehaviour
             {
                 GameObject go;
 
-                if (UnityEngine.Random.Range(0, 2000) % 2 == 0)
+                if (UnityEngine.Random.Range(0, 2000) % 5 == 0)
                 {
                     go = Instantiate(comboPrefabs[GetRandomPrefabType(comboPrefabs.Length)]);
                 }
@@ -55,12 +55,12 @@ public class FoodPlacer : MonoBehaviour
 
     private void UpdateTimerValueBasedOnScore()
     {
-        if (playerController.playerScore % 400 < 200 && playerController.playerScore % 400 >= 0)
+        if (playerController.playerScore % 500 < 250 && playerController.playerScore % 500 >= 0)
         {
-            timerMaxTime -= 0.02f;
+            timerMaxTime -= 0.05f;
 
-            if (timerMaxTime < 0.5f)
-                timerMaxTime = 0.5f;
+            if (timerMaxTime < 0.4f)
+                timerMaxTime = 0.4f;
         }
 
     }
