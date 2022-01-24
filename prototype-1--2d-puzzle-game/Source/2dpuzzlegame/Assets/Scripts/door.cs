a4171c7b6441e5fb4066369843b8aae945d90c37
Kamyab78 <amirhosseinghassemi78@yahoo.com>
2022-01-24 13:45:24 +0330

    Add:win menu

---
+++ prototype-1--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/door.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool touchDoor;
+    public GameObject WinMenu;
+    public GameObject Level;
+    public EventSystemCustom eventSystem;
+    private int keyEated;
+    void Start()
+    {
+        eventSystem.OnEatKeyEvent.AddListener(UpdateKeyEat);
+        keyEated = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (touchDoor)
+            {
+                if (keyEated == 3)
+                {
+                    WinMenu.SetActive(true);
+                    Level.SetActive(false);
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("User"))
+        {
+            touchDoor = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("User"))
+        {
+            touchDoor = false;
+        }
+    }
+    public void UpdateKeyEat()
+    {
+        keyEated++;
+    }
+}
