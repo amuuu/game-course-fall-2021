43f99fa00b66f9da94548381e247bdba450da1dd
Kamyab78 <amirhosseinghassemi78@yahoo.com>
2022-01-24 13:48:32 +0330

    Add:lose page

---
+++ prototype-1--2d-puzzle-game/Source/2dpuzzlegame/Assets/Scripts/lose.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lose : MonoBehaviour
+{
+    public GameObject losePage;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("User"))
+        {
+            losePage.SetActive(true);
+        }
+    }
+
+}
