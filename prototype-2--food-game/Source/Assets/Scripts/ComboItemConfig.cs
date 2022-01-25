b3e640138aa945b096131f93ff697d198c3e46dd
Sara <73617426+sarabarati@users.noreply.github.com>
2022-01-25 14:38:51 +0330

    Fix score

---
+++ prototype-2--food-game/Source/Assets/Scripts/ComboItemConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ComboItemConfig", menuName = "FoodGameConfigs/ComboItemConfig")]
+public class ComboItemConfig : ScriptableObject
+{
+    public string comboName;
+    public int heart;
+    public int weight;
+}
---
+++ prototype-2--food-game/Source/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(0f, 0.5f)] public float moveAmount;
+
+    public int playerScore;
+    public int playerHeartsCount;
+    public bool movePlate;
+    public Vector3 currentPosition;
+    public int counter;
+
+    private void Start()
+    {
+        playerScore = 0;
+        playerHeartsCount = 0;
+        counter = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(moveAmount, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-moveAmount, 0, 0);
+        }
+        if (FoodInstanceController.foodLoss == true)
+        {
+
+            counter += 1;
+            FoodInstanceController.foodLoss = false;
+            playerHeartsCount -= 1;
+            heart.heartAmount -= 1;
+        }
+        if (playerHeartsCount < 0)
+        {
+            SceneManager.LoadScene("game over");
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            // access the food object config
+            FoodItemConfig conf = collision.gameObject.GetComponent<FoodInstanceController>().config;
+
+            // increase the player's score
+            playerScore += conf.score;
+            score.scoreAmount += conf.score;
+
+            Debug.Log("SCORE: " + score.scoreAmount);
+
+            // destroy the food object
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Combo"))
+        {
+            // polymorphism!
+            // for example, the object of type "TimeFreezerComboController" which is the child of "ComboInstanceController", is put inside the "comboController" object below.
+            ComboInstanceController comboController = collision.gameObject.GetComponent<ComboInstanceController>();
+
+            // the CONTENT of OnConsume method inside "TimeFreezerComboController" is available inside the "comboController"
+
+            playerHeartsCount -= comboController.config.heart;
+            heart.heartAmount -= comboController.config.heart;
+
+            Debug.Log("HEARTS: " + playerHeartsCount);
+
+            Debug.Log("COMBO!!! " + comboController.config.comboName);
+
+            // destroy the combo object
+            Destroy(collision.gameObject);
+        }
+    }
+}
