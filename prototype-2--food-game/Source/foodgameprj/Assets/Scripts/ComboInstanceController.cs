5571bbffe09732bd59df8a1c49c56093e636b017
Sara <73617426+sarabarati@users.noreply.github.com>
2022-01-25 14:03:30 +0330

    Add game over

--- prototype-2--food-game/Source/foodgameprj/Assets/Scripts/ComboInstanceController.cs
+++ prototype-2--food-game/Source/foodgameprj/Assets/Scripts/ComboInstanceController.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class ComboInstanceController : MonoBehaviour
 {
     public ComboItemConfig config;
+    private Rigidbody rigidBody;
 
     // when player eats the combo item
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.mass = config.weight;
+        // rotate randomly when instantiating
+        transform.Rotate(0, Random.Range(-45, 45), 0);
+    }
+
     public virtual void OnConsume()
     {
         Debug.Log("PARENT CLASS ON CONSUME");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Kill"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- prototype-2--food-game/Source/foodgameprj/Assets/Scripts/PlayerController.cs
+++ prototype-2--food-game/Source/foodgameprj/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    [Range(0f, 1f)] public float moveAmount;
+    [Range(0f, 0.5f)] public float moveAmount;
 
     public int playerScore;
     public int playerHeartsCount;
+    public bool movePlate;
+    public Vector3 currentPosition;
+    public int counter;
 
     private void Start()
     {
         playerScore = 0;
+        playerHeartsCount = 0;
+        counter = 0;
     }
 
     void Update()
@@ -24,10 +30,24 @@ public class PlayerController : MonoBehaviour
         {
             transform.position += new Vector3(-moveAmount, 0, 0);
         }
+        if (FoodInstanceController.foodLoss == true)
+        {
+
+            counter += 1;
+            FoodInstanceController.foodLoss = false;
+            playerHeartsCount -= 1;
+            heart.heartAmount -= 1;
+        }
+        if (playerHeartsCount < 0)
+        {
+            SceneManager.LoadScene("game over");
+
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        
         if (collision.gameObject.CompareTag("Food"))
         {
             // access the food object config
@@ -35,8 +55,9 @@ public class PlayerController : MonoBehaviour
 
             // increase the player's score
             playerScore += conf.score;
+            score.scoreAmount += conf.score;
 
-            Debug.Log("SCORE: " + playerScore);
+            Debug.Log("SCORE: " + score.scoreAmount);
 
             // destroy the food object
             Destroy(collision.gameObject);
@@ -46,10 +67,14 @@ public class PlayerController : MonoBehaviour
         {
             // polymorphism!
             // for example, the object of type "TimeFreezerComboController" which is the child of "ComboInstanceController", is put inside the "comboController" object below.
-            ComboInstanceController comboController =  collision.gameObject.GetComponent<ComboInstanceController>();
+            ComboInstanceController comboController = collision.gameObject.GetComponent<ComboInstanceController>();
 
             // the CONTENT of OnConsume method inside "TimeFreezerComboController" is available inside the "comboController"
-            comboController.OnConsume();
+
+            playerHeartsCount -= comboController.config.heart;
+            heart.heartAmount -= comboController.config.heart;
+
+            Debug.Log("HEARTS: " + playerHeartsCount);
 
             Debug.Log("COMBO!!! " + comboController.config.comboName);
 
